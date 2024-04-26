978fbd0b2c15094b4bad4773e03bb6b816c94aa3
«Adil786000» <ai9069935@gmail.com>
2024-04-26 11:52:54 +0200

    He puesto los atributos y he hecho la Implementación del movimiento del fantasma

--- Adil-joc-PacMan-Java/fantasma.java
+++ Adil-joc-PacMan-Java/fantasma.java
@@ -2,5 +2,17 @@ public class Fantasma {
     private int posX;
     private int posY;
 
+    public Fantasma(int initialPosX, int initialPosY) {
+        this.posX = initialPosX;
+        this.posY = initialPosY;
+    }
+
+    public void move(int newPosX, int newPosY) {
+        
+    }
+
+    }       
     
+
+
 
