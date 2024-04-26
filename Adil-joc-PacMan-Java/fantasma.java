effcd62e516446e62ee75f0c63b3878ee9b81273
«Adil786000» <ai9069935@gmail.com>
2024-04-26 12:11:36 +0200

    Aqui he puesto la posicion del PACMAN.

--- Adil-joc-PacMan-Java/fantasma.java
+++ Adil-joc-PacMan-Java/fantasma.java
@@ -1,10 +1,13 @@
 public class Fantasma {
     private int posX;
     private int posY;
+    private String direction;
+
 
     public Fantasma(int initialPosX, int initialPosY) {
         this.posX = initialPosX;
         this.posY = initialPosY;
+        this.direction = "up";
     }
 
     public void move(int newPosX, int newPosY) {
