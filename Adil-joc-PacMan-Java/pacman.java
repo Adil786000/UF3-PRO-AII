d46ae7d0045132f4504aca865a5c87d080620e4c
«Adil786000» <ai9069935@gmail.com>
2024-04-26 10:36:39 +0200

    He puesto el enunciado.

---
+++ Adil-joc-PacMan-Java/pacman.java
@@ -0,0 +1,13 @@
+/*Requisits del joc:
+
+1.- Implementar les classes necessàries per representar els objectes del joc Pac-Man, incloent-hi:
+
+- PacMan: Representa el personatge principal del joc.
+        - Mètodes:
+            - move(int newPosX, int newPosY): Mou Pac-Man a una nova posició al laberint.
+            - pickupBola(): Recol·lecta una bola normal i augmenta la puntuació.
+            - pickupBolaEspecial(): Recol·lecta una bola especial i atorga poder temporal a Pac-Man.
+            - pickupFruita(): Recollita una fruita i augmenta la puntuació.
+            - loseLife(): Disminueix una vida quan Pac-Man és capturat per un fantasma.
+            - detectColisionFantasma(Fantasma fantasma): Verifica si Pac-Man col·lisiona amb un fantasma.*/
+            
