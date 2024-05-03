25763fe2d2ebbbba786786ee1dfff2d8a6a850aa
«Adil786000» <ai9069935@gmail.com>
2024-05-03 11:43:09 +0200

    He puesto los comentarios yt adémas he acabado el codigo de este archivo

--- Implementació d’un Arbre Binari en Java/BinaryTree.java
+++ Implementació d’un Arbre Binari en Java/BinaryTree.java
@@ -1,18 +1,21 @@
-public class BinaryTree {
+public class ArbolBinario {
     
     private static int instancias = 0;
-    private Node raiz;
+    private Nodo raiz;
     private int profundidad;
     
-    public BinaryTree(int profundidad) {
+    // Constructor de la clase ArbolBinario, incrementa el contador de instancias
+    public ArbolBinario(int profundidad) {
         instancias++;
         this.profundidad = profundidad;
     }
     
+    // Método estático para obtener el número de instancias creadas
     public static int getInstancias() {
         return instancias;
     }
     
+    // Método para crear un árbol de enteros con una rama derecha
     public void crearArbolEnteros() {
         this.raiz = new Nodo(1);
         Nodo actual = this.raiz;
@@ -22,10 +25,13 @@ public class BinaryTree {
         }
     }
     
+    // Método para crear un árbol de caracteres con una rama izquierda
     public void crearArbolCaracteres() {
         this.raiz = new Nodo('A');
-        Nodo actual = this.raiz
-        for (char c = 'B'; c < (char) ('A' + this.profundidad); c++){
+        Nodo actual = this.raiz;
+        for (char c = 'B'; c < (char) ('A' + this.profundidad); c++) {
             actual.izquierda = new Nodo(c);
             actual = actual.izquierda;
         }
+    }
+}
